8d32591d866ddf56d8dd0d411abc2f45fa407dac
Guido Pujadas <Guido_Pujadas@itresources.com.ar>
2019-06-26 10:55:12 -0300

    ADD - getFiles

--- src/components/fileListComponent.js
+++ src/components/fileListComponent.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
+/*import React, { useState, useEffect } from 'react';
 
 import axios from 'axios';
 
 const FileListComponent = () => {
-  let [files, setFiles] = useState([]);
+  const [files, setFiles] = useState([]);
 
-  useEffect(async () => {
+  useEffect(async () => (
       const result = await axios.get('http://localhost:3002/files')
       console.log(result)
       console.log("fds");
@@ -13,12 +13,49 @@ const FileListComponent = () => {
   }, []);
 
   return (
+    <div>
     <ul>
       {files.map(file => {
         <li key={file.objectID}>
           <a href={file.fileName}>{file.fileTitle}</a>
         </li>
       })}
+    </ul>
+    </div>
+  );
+}
+
+export default FileListComponent;
+
+*/
+
+const baseURL = 'http://localhost:3003'
+
+//import React, { useState, useEffect } from 'react';
+//import axios from 'axios';
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function FileListComponent() {
+  const [data, setData] = useState( [] );
+
+  useEffect(async () => {
+    const result = await axios(
+      `${baseURL}/files`,
+    );
+    console.log("result")
+    console.log(result.data.result)
+    setData(result.data.result);
+  }, []);
+
+  return (
+    <ul>
+      {data.map(item => (
+        <li key={item.objectID}>
+          <a href={item.objectID}>{item.fileName}</a>
+        </li>
+      ))}
     </ul>
   );
 }
